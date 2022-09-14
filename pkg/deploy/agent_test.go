d8a132415024bd22d7a855fdfb26ce2e7d224aac
Aleksandar Savchev <aleksandar.savchev@sap.com>
2022-09-14 13:29:04 +0300

    Added DefaultSeccompProfileEnabled flag and tests

---
+++ pkg/deploy/agent_test.go
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package deploy_test
+
+import (
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/gardener/network-problem-detector/pkg/deploy"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Add default seccomp profile when enabled", func() {
+	var deployConfig *deploy.AgentDeployConfig
+
+	BeforeEach(func() {
+		deployConfig = &deploy.AgentDeployConfig{
+			Image:                    "image:tag",
+			DefaultPeriod:            16 * time.Second,
+			PodSecurityPolicyEnabled: true,
+			PingEnabled:              false,
+		}
+	})
+
+	It("should create daemonset without seccomp profile", func() {
+		objs, err := deploy.DeployNetworkProblemDetectorAgent(deployConfig)
+		Expect(err).To(BeNil())
+		Expect(len(objs)).NotTo(BeZero())
+		var ds *appsv1.DaemonSet
+		for _, obj := range objs {
+			switch v := obj.(type) {
+			case *appsv1.DaemonSet:
+				ds = v
+			}
+		}
+
+		Expect(ds).NotTo(BeNil())
+		Expect(ds.Spec.Template.Spec.SecurityContext).To(BeNil())
+	})
+
+	It("should create daemonset with seccomp profile", func() {
+		deployConfig.DefaultSeccompProfileEnabled = true
+		objs, err := deploy.DeployNetworkProblemDetectorAgent(deployConfig)
+		Expect(err).To(BeNil())
+		Expect(len(objs)).NotTo(BeZero())
+		var ds *appsv1.DaemonSet
+		for _, obj := range objs {
+			switch v := obj.(type) {
+			case *appsv1.DaemonSet:
+				ds = v
+			}
+		}
+
+		Expect(ds).NotTo(BeNil())
+		Expect(ds.Spec.Template.Spec.SecurityContext).NotTo(BeNil())
+		Expect(ds.Spec.Template.Spec.SecurityContext.SeccompProfile.Type).To(Equal(corev1.SeccompProfileTypeRuntimeDefault))
+	})
+})
